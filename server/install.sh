2882581d1f48c23d2c6e0921cf60c4f859c0dd94
Cody Childers <cchilder@mail.usf.edu>
2017-10-19 23:18:15 -0500

    add install pip to installer

--- server/install.sh
+++ server/install.sh
@@ -1,7 +1,7 @@
 
-# sudo apt install pip
-# how to install pip again?
-sudo pip install virtualenv
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
 virtualenv venv
 ./venv/bin/pip install -r requirements.txt
 ./venv/bin/python server.py
